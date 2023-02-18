633f63629cff4edbc5662ff11f2317d123a36ad7
adnanabrar786 <adnan1245abrar@gmail.com>
2023-02-18 13:54:12 -0800

    commit

--- firstclass/firstclass.js
+++ firstclass/firstclass.js
@@ -1,40 +1,74 @@
-
-
-// const http = require("http")
+// const http = require("http");
 // const fs = require("fs");
-// const path = require("path")
+// const path = require("path");
 
-// const filePath = path.join(__dirname, "data.text")
+// // const filePath = path.join(__dirname, "data", "data.text");
+// const filePath = path.join(__dirname, "data.text");
 
 // const server = http.createServer((req, res) => {
-//     if (req.url === "/") {
-//         console.log(req.url);
-//         res.write("hello Motrolla")
-//         res.end()
-//     } else if (req.url === "/form") {
-//         res.setHeader("Content-Type", "text/html");
-//         res.write("<form action='/submit' method='POST'><input name='data'/><input name='data2'/> <button>submit</button> </form>")
-//         res.end()
-//     } else if (req.url === "/submit") {
-//         let data = "";
-//         req.on("data", (chunk) => {
-//             data += chunk;
+//   if (req.url === "/") {
+//     console.log(req.url);
+//     res.write("hello Motrolla");
+//     res.end();
+//   } else if (req.url === "/form") {
+//     res.setHeader("Content-Type", "text/html");
+//     res.write(
+//       "<form action='/submit' method='POST'><input name='data'/><input name='data2'/> <button>submit</button> </form>"
+//     );
+//     res.end();
+//   } else if (req.url === "/submit") {
+//     let data = "";
+//     req.on("data", (chunk) => {
+//       data += chunk;
+//     });
+//     req.on("end", () => {
+//       fs.readFile(filePath, "utf8", (err, oldData) => {
+//         const newData = oldData + "/n" + data;
+//         fs.writeFile(filePath, newData, () => {
+//           console.log("saved");
 //         });
-//         req.on("end", () => {
-//             fs.readFile(filePath, (err , oldData) => {
-//                 const newData = oldData + "/n" + data ;
-//                 fs.writeFile(filePath, newData, () => {
-//                     console.log("saved");
-//                 })
-//             })
-//         });
-//         res.write("Data Received")
-//         res.end();
-//     }
-// })
+//       });
+//     });
+//     res.write("Data Received");
+//     res.end();
+//   }
+// });
 
 // server.listen(3000);
 
 
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
- 
+const filePath = path.join(__dirname, "data.txt");
+
+const server = http.createServer((req, res) => {
+    if (req.url === "/") {
+        res.write("Hello");
+        res.write("World");
+        res.end();
+    } else if (req.url === "/form") {
+        res.setHeader("Content-Type", "text/html");
+        res.write("<form action='/submit' method='POST'><input name='data' /><input name='data2' /><button>Submit</button></form>");
+        res.end();
+    } else if (req.url === "/submit") {
+        let data = "";
+        req.on("data", (chunk) => {
+            data += chunk;
+        });
+        req.on("end", () => {
+            fs.readFile(filePath, 'utf8', (err, oldData) => {
+                const newData = oldData + "\n" + data;
+                fs.writeFile(filePath, newData, () => {
+                    console.log("Saved");
+                });
+            });
+        });
+        res.write("Data Received");
+        res.end();
+    }
+
+});
+
+server.listen(3000);
