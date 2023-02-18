0c88b536cb2ddff6ef0587cd11c63a48173eda0f
adnanabrar786 <adnan1245abrar@gmail.com>
2023-02-18 13:57:18 -0800

    commit

--- firstclass/firstclass.js
+++ firstclass/firstclass.js
@@ -1,41 +1,4 @@
-// const http = require("http");
-// const fs = require("fs");
-// const path = require("path");
-
-// // const filePath = path.join(__dirname, "data", "data.text");
-// const filePath = path.join(__dirname, "data.text");
-
-// const server = http.createServer((req, res) => {
-//   if (req.url === "/") {
-//     console.log(req.url);
-//     res.write("hello Motrolla");
-//     res.end();
-//   } else if (req.url === "/form") {
-//     res.setHeader("Content-Type", "text/html");
-//     res.write(
-//       "<form action='/submit' method='POST'><input name='data'/><input name='data2'/> <button>submit</button> </form>"
-//     );
-//     res.end();
-//   } else if (req.url === "/submit") {
-//     let data = "";
-//     req.on("data", (chunk) => {
-//       data += chunk;
-//     });
-//     req.on("end", () => {
-//       fs.readFile(filePath, "utf8", (err, oldData) => {
-//         const newData = oldData + "/n" + data;
-//         fs.writeFile(filePath, newData, () => {
-//           console.log("saved");
-//         });
-//       });
-//     });
-//     res.write("Data Received");
-//     res.end();
-//   }
-// });
-
-// server.listen(3000);
-
+// fs  --> File System
 
 const http = require("http");
 const fs = require("fs");
